89fe4c586c0e4b257d803ab0a7d165a3916939a6
Santiago <santi.nico.herrera@gmail.com>
2025-12-15 22:54:54 -0300

    Arreglo error ordenamiento sin return

--- src/service/AnimeServiceImpl.java
+++ src/service/AnimeServiceImpl.java
@@ -1,17 +1,12 @@
 package service;
 
 import model.*;
-import repository.IAnimeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnimeServiceImpl implements IAnimeService {
-    private final IAnimeRepository animeRepository;
-
-    public AnimeServiceImpl(IAnimeRepository animeRepository) {
-        this.animeRepository = animeRepository;
-    }
+    List<Anime> animes = new ArrayList<>();
 
     @Override
     public void crearAnime(
@@ -23,9 +18,6 @@ public class AnimeServiceImpl implements IAnimeService {
             int calificacion,
             List<Generos> generos
     ) throws ValidacionException {
-        if (!animeRepository.obtenerAnimesPorTitulo(titulo).isEmpty()) {
-            throw new AnimeYaExisteException("Anime " + titulo + ", ya existe");
-        }
         Anime nuevoAnime = new Anime(titulo,
                 anioLanzamiento,
                 estudio,
@@ -33,67 +25,23 @@ public class AnimeServiceImpl implements IAnimeService {
                 calificacion,
                 generos,
                 cantidadCapitulos);
-        animeRepository.guardarAnime(nuevoAnime);
+        animes.add(nuevoAnime);
+        // llamar al repository aca para guardar el nuevo anime
     }
 
     @Override
     public List<Anime> obtenerTodos() {
-        return this.animeRepository.listarAnimes();
+        // deberia obtener todos los animes del repository
+        return animes;
     }
 
     @Override
     public List<Anime> ordenarAnimes(List<Anime> animes, CriterioOrdenamiento criterio, Ordenamientos orden) {
-        return criterio.ordenar(this.animeRepository.listarAnimes(), orden);
+        return criterio.ordenar(this.animes, orden);
     }
 
     @Override
     public List<Anime> filtrar(CriterioFiltrado criterio) {
-        return criterio.filtrar(this.animeRepository.listarAnimes());
+        return criterio.filtrar(this.animes);
     }
-
-    @Override
-    public Double obtenerPromedioCalificacionesGlobal() {
-        List<Anime> animes = this.animeRepository.listarAnimes();
-        int sumaCalificacionesTotal = 0;
-
-        for (int i = 0; i <= animes.size() ; i++) {
-            sumaCalificacionesTotal += animes.get(i).getCalificacionUsuarios();
-        }
-        return (double) (sumaCalificacionesTotal / animes.size());
-    }
-
-    @Override
-    public List<Generos> top3GenerosMasFrecuentes() {
-        Generos[] generos = Generos.values();
-        List<Integer> cantidades = new ArrayList<>();
-
-        for (Generos genero : generos) {
-            int amount = this.animeRepository.obtenerAnimesPorGenero(genero).size();
-            cantidades.add(amount);
-        }
-
-        List<Generos> top3 = new ArrayList<>();
-
-        for (int k = 0; k < 3 && k < generos.length; k++) {
-            int maxIndex = -1;
-            int maxCantidad = -1;
-            for (int i = 0; i < cantidades.size(); i++) {
-                if (cantidades.get(i) > maxCantidad) {
-                    maxCantidad = cantidades.get(i);
-                    maxIndex = i;
-                }
-            }
-            if (maxIndex != -1 && maxCantidad > 0) {
-                top3.add(generos[maxIndex]);
-                cantidades.set(maxIndex, -1);
-            }
-        }
-        return top3;
-    }
-
-    @Override
-    public List<Anime> recomendar(CriterioRecomendacion criterio, int n) {
-        return criterio.recomendar(n);
-    }
-
 }
